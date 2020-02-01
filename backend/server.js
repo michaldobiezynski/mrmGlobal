78f1a984a6445189e4089809517c17ac680a97d7
michaldobiezynski <dobiezynski@gmail.com>
2020-02-01 11:19:12 +0000

    connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res, next) => res.send('Api running'));
 
