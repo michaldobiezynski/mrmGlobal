29e3acd8d7b817a80fc1d877c7cae6676b093a16
michaldobiezynski <dobiezynski@gmail.com>
2020-02-01 14:53:46 +0000

    cleaned

--- backend/migration/jsonToDB.js
+++ backend/migration/jsonToDB.js
@@ -1,9 +1,41 @@
 const fs = require('fs');
+const mongoose = require('mongoose');
 
-const Drink = require('../models/Drink');
+mongoose.connect(
+  'mongodb+srv://michal123:michal123@devconnector-6tf9d.mongodb.net/mrmGlobal?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+);
 
-let productsData = fs.readFile('../data/products.json');
+const DrinkSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+const Drink = mongoose.model('drink', DrinkSchema);
+
+let productsData = fs.readFileSync('../data/products.json');
 
 let products = JSON.parse(productsData);
 
-Drink.insertMany(products);
+Drink.insertMany(products, (error, prodcuts) => {
+  if (error) {
+    console.error(error.message);
+    mongoose.disconnect();
+  } else {
+    console.info(
+      '%d drinks were successfully added to the database',
+      products.length,
+    );
+    mongoose.disconnect();
+  }
+});
